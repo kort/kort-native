b44ddf35f98b473e8e3ef786276775d269bb7177
Andreas Egloff <egloff.andreas@gmail.com>
2017-01-11 15:43:48 +0100

    init react native project

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+    View
+} from 'react-native';
+
+class App extends Component {
+
+    render() {
+        return (
+            <View />
+        );
+    }
+}
+
+export default App;
