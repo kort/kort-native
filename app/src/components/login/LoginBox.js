73f5767f9bbb4106e4f2d8f1ae0f55a894c7e540
Andreas Egloff <egloff.andreas@gmail.com>
2017-01-15 17:12:56 +0100

    provider selection available

--- app/src/components/login/LoginBox.js
+++ app/src/components/login/LoginBox.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import {
     View,
+    Text,
     Image,
     KeyboardAvoidingView
 } from 'react-native';
+import ModalDropdown from 'react-native-modal-dropdown';
 import { 
      Button,
      Input
 } from '../common';
 
-const LoginBox = ({ onLogin }) => {
+
+const LoginBox = ({ onLogin, options, onOptionSelect, selectedOption }) => {
         return (
             <KeyboardAvoidingView style={styles.loginBoxStyle} behavior='position'>
                 <Image 
                     source={require('../../../assets/images/login/kortLogo.png')} 
-                    style={styles.imageStyle}
+                    style={styles.imageLogoStyle}
                 />
                 <Input
                     imgSource={require('../../../assets/images/tabIcons/profile.png')}
@@ -25,7 +28,18 @@ const LoginBox = ({ onLogin }) => {
                     placeHolder={'password'}
                     secureTextEntry
                 />
-
+                <View style={styles.dropdownStyle}>
+                <Image 
+                        style={styles.providerImageStyle}
+                        source={require('../../../assets/images/login/cloud.png')}
+                />
+                    <ModalDropdown options={options} onSelect={onOptionSelect}>
+                        <Image 
+                            source={selectedOption}
+                            style={styles.imageOptionStyle} 
+                        />
+                    </ModalDropdown>
+                </View>
                 <Button onPress={onLogin} style={styles.buttonStyle}>
                     Login
                     </Button>
@@ -34,18 +48,35 @@ const LoginBox = ({ onLogin }) => {
 };
 
 const styles = {
+    dropdownStyle: {
+        marginTop: 10,
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
     loginBoxStyle: {
         justifyContent: 'flex-end',
-         backgroundColor: 'transparent'
+        backgroundColor: 'transparent',
     },
-     imageStyle: {
+     imageLogoStyle: {
          marginBottom: 30,
          width: 300,
          height: 300
      },
+    imageOptionStyle: {
+         width: 30,
+         height: 30
+     },
      buttonStyle: {
          justifyContent: 'flex-end',
-         marginTop: 30
+         marginTop: 30,
+         marginBottom: 10
+     },
+     providerImageStyle: {
+        paddingLeft: 20,
+        marginRight: 10,
+        height: 30,
+        width: 30,
+        tintColor: '#657C8E'
      }
 };
 
--- app/src/components/login/LoginOverview.js
+++ app/src/components/login/LoginOverview.js
@@ -7,7 +7,14 @@ import LoginBox from './LoginBox';
 
 class LoginOverview extends Component {
 
-    state = { showModal: true, animationType: 'none' };
+    state = { 
+        showModal: true, 
+        animationType: 'none', 
+        providers: ['Google', 'Facebook', 'OSM'],
+        selectedProvider: 'Google',
+        selectedProviderImage: require('../../../assets/images/login/google.png')
+    };
+
 
     onLogin() {
         this.setState({ showModal: false });
@@ -15,6 +22,23 @@ class LoginOverview extends Component {
 
     onShow() {
         this.setState({ animationType: 'slide' });
+    }
+
+    onOptionSelect(index, value) {
+        switch (value) {
+
+            case 'Google':
+                this.setState({ selectedProviderImage: require('../../../assets/images/login/google.png') });
+                break;
+            case 'Facebook':
+                this.setState({ selectedProviderImage: require('../../../assets/images/login/facebook.png') });
+                break;
+            case 'OSM':
+                this.setState({ selectedProviderImage: require('../../../assets/images/login/osm.png') });
+                break;
+            default:
+                this.setState({ selectedProviderImage: '' });
+        }
     }
     
     render() {
@@ -26,8 +50,12 @@ class LoginOverview extends Component {
                     onRequestClose={() => {}}
                 >
                     <View style={styles.backgroundStyle} >
-                        <LoginBox onLogin={this.onLogin.bind(this)} />
-
+                        <LoginBox
+                            onLogin={this.onLogin.bind(this)}
+                            options={this.state.providers}
+                            onOptionSelect={this.onOptionSelect.bind(this)}
+                            selectedOption={this.state.selectedProviderImage}
+                        />
                     </View>
          </Modal>
         );
