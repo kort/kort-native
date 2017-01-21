4dab460651106ebb6dfa717a95dd8d18af59b6dc
Andreas Egloff <egloff.andreas@gmail.com>
2017-01-21 12:52:33 +0100

    Google SignIn added in RN

--- app/ios/app/AppDelegate.m
+++ app/ios/app/AppDelegate.m
@@ -8,6 +8,7 @@
  */
 
 #import "AppDelegate.h"
+#import <RNGoogleSignin/RNGoogleSignin.h>
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -32,6 +33,12 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+  
+  return [RNGoogleSignin application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
 }
 
 @end
