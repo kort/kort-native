6cb755d68be044a135da1741a83e32813748b207
Andreas Egloff <egloff.andreas@gmail.com>
2017-01-26 16:02:23 +0100

    added host for testing backend

--- app/src/constants/Config.js
+++ app/src/constants/Config.js
@@ -2,8 +2,13 @@ import SecretConfig from './SecretConfig';
 
 export default {
 
+  API_URL: 'http://a04df801.ngrok.io',
+
   GOOGLE: 'google',
   GOOGLE_IOS_CLIENT_ID: SecretConfig.GOOGLE_IOS_CLIENT_ID,
-  GOOGLE_WEB_CLIENT_ID: SecretConfig.GOOGLE_WEB_CLIENT_ID
+  GOOGLE_WEB_CLIENT_ID: SecretConfig.GOOGLE_WEB_CLIENT_ID,
+
+  GOOGLE_VERIFY: '/google/verify',
+  OSM_LOGIN: '/osm/login'
 
 };
