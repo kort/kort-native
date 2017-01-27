2ef2df433b9b9c7c2c177e17482f54d623b8ad72
Andreas Egloff <egloff.andreas@gmail.com>
2017-01-27 20:03:43 +0100

    fixed #2, auth to user info

--- app/src/data/KortAPI.js
+++ app/src/data/KortAPI.js
@@ -2,12 +2,23 @@ import RestClient from 'react-native-rest-client';
 import Config from '../constants/Config';
 
 export default class KortAPI extends RestClient {
-    constructor() {
-    super(Config.API_URL);
+
+    constructor(authToken) {
+    super(Config.API_URL, {
+      headers: {
+        Authorization: `${authToken}`
+      },
+      simulatedDelay: 2000
+    });
   }
 
   verifyUser(tokenId) {
     return this.POST(Config.GOOGLE_VERIFY, { tokenId });
+  }
+
+  getUserinfo(userId) {
+    console.log(`${Config.USER_INFO}/${userId}`);
+    return this.GET(`${Config.USER_INFO}/${userId}`);
   }
  
 }
