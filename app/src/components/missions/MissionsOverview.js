30459e02005f44bb9f29f92c2221088d0e0f9c20
Andreas Egloff <egloff.andreas@gmail.com>
2017-02-03 09:37:11 +0100

    loading spinner on map /

--- app/src/components/missions/MissionsOverview.js
+++ app/src/components/missions/MissionsOverview.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import {
     View
 } from 'react-native';
+import { connect } from 'react-redux';
 import Map from './Map';
+import { Spinner } from '../common';
 
 class MissionsOverview extends Component {
 
@@ -10,17 +12,39 @@ class MissionsOverview extends Component {
         return (
             <View style={styles.bgColor}>
                 <Map />
+                <Spinner
+                    size='small'
+                    style={this.props.mapModeFullScreen ? styles.spinnerStyleFullScreen : styles.spinnerStyleSmallScreen} 
+                />
             </View>
         );
     }
 }
 
 const styles = {
+    spinnerStyleFullScreen: {
+        position: 'absolute',
+        paddingTop: 20,
+        paddingLeft: 10
+    },
+    spinnerStyleSmallScreen: {
+        position: 'absolute',
+        paddingTop: 74,
+        paddingBottom: 60,
+        paddingLeft: 10
+    },
     bgColor: {
         backgroundColor: '#202931',
-        flex: 1
+        flex: 1,
     }
 };
 
 
-export default MissionsOverview;
+const mapStateToProps = ({ mapReducer }) => {
+    console.log(mapReducer);
+    const { mapModeFullScreen } = mapReducer;
+    return { mapModeFullScreen };
+};
+
+
+export default connect(mapStateToProps, {})(MissionsOverview);
