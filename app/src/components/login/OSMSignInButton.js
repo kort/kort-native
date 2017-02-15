40c851b806fb2802c1814824abbdc9ccd6ddccd9
Andreas Egloff <egloff.andreas@gmail.com>
2017-02-15 12:16:24 +0100

    cleanup

--- app/src/components/login/OSMSignInButton.js
+++ app/src/components/login/OSMSignInButton.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import {
-    TextInput,
-    View,
-    Image,
-    Linking
-} from 'react-native';
 import { ButtonWithImage } from './../common';
 
 const OSMSignInButton = ({ onPress }) => {
