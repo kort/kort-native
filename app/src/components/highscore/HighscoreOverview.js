8d179c557b92463786d9d25bb3d54c1b58992039
Andreas Egloff <egloff.andreas@gmail.com>
2017-02-21 11:31:04 +0100

    tabs in highscore view

--- app/src/components/highscore/HighscoreOverview.js
+++ app/src/components/highscore/HighscoreOverview.js
@@ -1,20 +1,52 @@
 import React, { Component } from 'react';
 import {
-    View
+    View,
+    Platform
 } from 'react-native';
+import SegmentedControlTab from 'react-native-segmented-control-tab';
 
 class HighscoreOverview extends Component {
     render() {
         return (
-            <View style={styles.bgColor} />
+            <View style={styles.bgColor} >
+                    <SegmentedControlTab
+                    tabsContainerStyle={styles.tabsContainerStyle}
+                    tabStyle={styles.tabStyle}
+                    activeTabStyle={styles.activeTabStyle}
+                    tabTextStyle={styles.tabTextStyle}
+                    values={['Day', 'Week', 'Month', 'Year']}
+                    onTabPress={index => console.log(index)}
+                    />
+            </View>
         );
     }
 }
 
 const styles = {
     bgColor: {
+        marginTop: (Platform.OS === 'ios') ? 64 : 54,
         backgroundColor: '#202931',
         flex: 1
+    },
+    tabsContainerStyle: {
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderColor: '#395971',
+        height: 60,
+    },
+    tabTextStyle: {
+        color: '#395971',
+        backgroundColor: 'transparent'        
+    },
+    tabStyle: {
+        backgroundColor: 'white',
+        borderColor: '#395971',
+        borderWidth: (Platform.OS === 'ios') ? 0 : 1,
+    },
+    activeTabStyle: {
+        backgroundColor: '#395971'
     }
 };
 
