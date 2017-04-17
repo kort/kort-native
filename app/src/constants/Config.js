293ee48ef1c8f5cd4faf34f12042168fe169ba13
Andreas Egloff <egloff.andreas@gmail.com>
2017-04-17 16:11:26 +0200

    updated settings for testing

--- app/src/constants/Config.js
+++ app/src/constants/Config.js
@@ -11,7 +11,7 @@ export default {
   MAPBOX_INITIAL_ZOOM_LEVEL: 14,
   MAPBOX_ANNOTATION_SIZE: 30,
 
-  API_URL: 'http://689f3a55.ngrok.io',
+  API_URL: 'http://e2ad5d2d.ngrok.io',
 
   DEEP_LINK_URL: 'kortapp://payload?',
 
