20020f47c37f965abf9d23f403ee427186395b64
Andreas Egloff <egloff.andreas@gmail.com>
2017-05-06 09:51:26 +0200

    oh: verifier initalized

---
+++ app/src/date/OpenHoursVerifier.js
@@ -0,0 +1,22 @@
+// import oh from 'opening_hours ';
+
+// /**
+//  * this helper library verifies any oh string
+//  * with the help of the opening_hours library
+//  * @param {*} entries 
+//  */
+// const OpeningHoursVerifier = (oh_string) => {
+//     return (
+//         verifyOHRepresentation(oh_string)
+//     );
+// };
+
+
+// function verifyOHRepresentation(oh_string) {
+//     var oh = new opening_hours(oh_string);
+
+//     return true;
+// }
+
+
+// export default OpeningHoursVerifier;
