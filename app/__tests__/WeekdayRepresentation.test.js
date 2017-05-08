6ebc375aa3ad0f6af1db9fec64a7e5323d7adb09
Andreas Egloff <egloff.andreas@gmail.com>
2017-05-08 11:26:54 +0200

    weekday representation test added

---
+++ app/__tests__/WeekdayRepresentation.test.js
@@ -0,0 +1,37 @@
+import WeekdayRepresentation from '../src/date/WeekdayRepresentation';
+
+test('test empty weekdays', () => {
+    const weekdays = [];
+    const result = WeekdayRepresentation(weekdays);
+  expect(result).toBe('');
+});
+
+test('test workdays series', () => {
+    const weekdays = [0, 1, 2, 3, 4];
+    const result = WeekdayRepresentation(weekdays);
+  expect(result).toBe('Mo-Fr');
+});
+
+test('test over the weekend range', () => {
+    const weekdays = [4, 5, 6, 0];
+    const result = WeekdayRepresentation(weekdays);
+  expect(result).toBe('Fr-Mo');
+});
+
+test('test two value representation', () => {
+    const weekdays = [5, 6];
+    const result = WeekdayRepresentation(weekdays);
+  expect(result).toBe('Sa,Su');
+});
+
+test('test consecutive days mixed with single values', () => {
+    const weekdays = [0, 1, 2, 5];
+    const result = WeekdayRepresentation(weekdays);
+  expect(result).toBe('Mo-We,Sa');
+});
+
+test('test over the weekend range mixed with single values', () => {
+    const weekdays = [2, 4, 5, 6, 0];
+    const result = WeekdayRepresentation(weekdays);
+  expect(result).toBe('We,Fr-Mo');
+});
